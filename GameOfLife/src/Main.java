a52ea75d36931fec45887f5db2333343ca709c94
Ilia <ilya.pustograev.i@mail.ru>
2023-11-27 16:44:49 +0300

    docs: add some comments

--- GameOfLife/src/Main.java
+++ GameOfLife/src/Main.java
@@ -20,6 +20,7 @@ public class Main {
         int fullCellsRowIndex = 0;
         int fullCellsColumnIndex = 1;
 
+        //Генерируем координаты полных ячеек
         for (int i = 0; i < fullCells; i += 1) {
             int randomRow = ThreadLocalRandom.current().nextInt(0, rows);
             int randomColumn = ThreadLocalRandom.current().nextInt(0, columns);
@@ -36,6 +37,7 @@ public class Main {
         char emptyCell = 46; //symbol . = 46;
         char fullCell = 42; //symbol * = 42;
 
+        //Инициализируем поле, заполняя его пустыми ячейками
         for (int i = 0; i < rows; i += 1) {
             for (int j = 0; j < columns; j += 1) {
                 //field[i][j] = emptyCell;
@@ -48,6 +50,7 @@ public class Main {
             }
         }
 
+        //Добавляем на поле полные ячейки по сгенерированным ранее координатам
         for (int i = 0;  i < fullCells; i += 1) {
             int x = fullCellsPosition[i][fullCellsRowIndex];
             int y = fullCellsPosition[i][fullCellsColumnIndex];
@@ -57,6 +60,8 @@ public class Main {
             field[x][y].symbol = fullCell;
         }
 
+        //Выводим получившееся поле на экран
+        //TODO: сделать в этом же цикле изначальные подсчет живых соседей и вычисление будет ли ячейка жива в следующей генерации
         for (int i = 0; i < rows; i += 1) {
             for (int j = 0; j < columns; j += 1) {
                 System.out.print(field[i][j].symbol);
