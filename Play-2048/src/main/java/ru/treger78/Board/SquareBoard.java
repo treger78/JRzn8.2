3c63eb30ad0816592d46e721015a8c93c4f5c1cd
Ilia <ilya.pustograev.i@mail.ru>
2023-12-06 14:26:26 +0300

    doc: add comments to implement

--- Play-2048/src/main/java/ru/treger78/Board/SquareBoard.java
+++ Play-2048/src/main/java/ru/treger78/Board/SquareBoard.java
@@ -9,6 +9,7 @@ public class SquareBoard extends Board {
         super(size, size);
     }
 
+    //Заполняем поле элементами из list, если нужно задать пустой элемент, указываем null
     @Override
     public void fillBoard(List<Integer> list) {
         for (int i = 0; i < board.size(); i += 1) {
@@ -18,41 +19,49 @@ public class SquareBoard extends Board {
         }
     }
 
+    //Возвращает все ключи, у которых значение null
     @Override
     public List<Key> availableSpace() {
         return null;
     }
 
+    //Добавляет элемент value по ключу key
     @Override
     public void addItem(Key key, Integer value) {
-
+        board.put(key, value);
     }
 
+    //Ищем уже существующий ключ по координатам
     @Override
     public Key getKey(int i, int j) {
-        return null;
+        return new Key(i ,j);
     }
 
+    //Получаем значение по key
     @Override
     public Integer getValue(Key key) {
         return null;
     }
 
+    //Получаем столбец ключей по которым потом можно выбрать значения
     @Override
     public List<Key> getColumn(int j) {
         return null;
     }
 
+    //Аналогично для строчки
     @Override
     public List<Key> getRow(int i) {
         return null;
     }
 
+    //Проверяем есть ли такое значение на поле
     @Override
     public boolean hasValue(Integer value) {
         return false;
     }
 
+    //Получаем строчку значений по строке ключей
     @Override
     public List<Integer> getValues(List<Key> keys) {
         return null;
--- Play-2048/src/main/java/ru/treger78/Game/Game2048.java
+++ Play-2048/src/main/java/ru/treger78/Game/Game2048.java
@@ -15,31 +15,37 @@ public class Game2048 implements Game {
         this.board = board;
     }
 
+    //Инициирует начало игры
     @Override
     public void init() {
 
     }
 
+    //Проверяет возможно ли сделать ход
     @Override
     public boolean canMove() {
         return false;
     }
 
+    //Делает ход в направлении
     @Override
     public boolean move(Direction direction) {
         return false;
     }
 
+    //Добавляет новый элемент в игру
     @Override
     public void addItem() {
 
     }
 
+    //Получение игрового поля
     @Override
     public Board getGameBoard() {
         return null;
     }
 
+    //Случилось ли событие победы
     @Override
     public boolean hasWin() {
         return false;
