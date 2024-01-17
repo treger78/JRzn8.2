23c567dede9aa4d9ac7a4de6ba9ba2e305668806
Ilia <my@mail.com>
2024-01-17 20:22:32 +0300

    feat

--- Jackson/src/main/java/org/example/Main.java
+++ Jackson/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public void whenJavaGotFromXmlFile_thenCorrect() throws IOException {
+        File file = new File("ClientInfo.xml");
+        XmlMapper xmlMapper = new XmlMapper();
+        ClientInfo value = xmlMapper.readValue(file, ClientInfo.class);
     }
 }
